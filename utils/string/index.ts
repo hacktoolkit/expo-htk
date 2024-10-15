ecdd6055c391998e3b4a98b10f0f9a0faeb2fb91
Yosef <jossyfreelancer@gmail.com>
2024-10-15 23:11:04 +0300

    remove pluralizer

--- utils/string/index.ts
+++ utils/string/index.ts
@@ -1,37 +1,3 @@
-export function pluralize(
-    word: string,
-    count: number,
-    customSuffix: string = '',
-): string {
-    let pluralWord = word;
-
-    if (count !== 1) {
-        if (customSuffix) {
-            pluralWord += customSuffix;
-        } else {
-            const lastChar = word.charAt(word.length - 1);
-            const lastTwoChars = word.slice(-2);
-            let suffix = 's';
-
-            if (
-                ['s', 'x', 'z'].includes(lastChar) ||
-                ['sh', 'ch'].includes(lastTwoChars)
-            ) {
-                suffix = 'es';
-            } else if (
-                lastChar === 'y' &&
-                !['a', 'e', 'i', 'o', 'u'].includes(word.charAt(word.length - 2))
-            ) {
-                pluralWord = word.slice(0, -1) + 'i';
-                suffix = 'es';
-            }
-            pluralWord += suffix;
-        }
-    }
-
-    return pluralWord;
-}
-
 export function truncateWord(text: string, wordCount: number): string {
     const words = text.split(' ');
 
