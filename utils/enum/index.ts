82a5f9594d1c9c412369b7a191a8958448dc908d
Yosef <jossyfreelancer@gmail.com>
2024-10-16 11:14:10 +0300

    update to minimal change

--- utils/enum/index.ts
+++ utils/enum/index.ts
@@ -1,34 +1,6 @@
 import { snakeCaseToCapitalize } from '../string';
 
-export type Enum<E> = Record<keyof E, number | string> & { [k: number]: string };
-
-export function enumToSelectOptions<E extends Enum<E>>(
-    enumObj: E,
-    hiddenValues: number[] = []
-): { label: string; value: number }[] {
-    return Object.entries(enumObj)
-        .filter(
-            ([, value]) => typeof value === 'number' && !hiddenValues.includes(value)
-        )
-        .map(([label, value]) => ({
-            label: snakeCaseToCapitalize(label),
-            value: value as unknown as number,
-        }));
-}
-
 export function enumToStr(enumObj: any, val: number): string {
     let name = enumObj[val] as string;
     return snakeCaseToCapitalize(name);
-}
-
-export function enumNameList<E extends Enum<E>>(enumObj: E, value: number): string[] {
-    const names: string[] = [];
-
-    for (const key in enumObj) {
-        const k = key as keyof typeof enumObj;
-        if (value & (enumObj[k] as number)) {
-            names.push(snakeCaseToCapitalize(k as string));
-        }
-    }
-    return names;
 }
--- utils/string/index.ts
+++ utils/string/index.ts
@@ -1,11 +1,3 @@
-export function truncateWord(text: string, wordCount: number): string {
-    const words = text.split(' ');
-
-    return words.length > wordCount
-        ? words.slice(0, wordCount).join(' ') + '...'
-        : words.join(' ');
-}
-
 export function capitalize(name: string): string {
     return `${name.charAt(0).toUpperCase()}${name.slice(1).toLowerCase()}`;
 }
