da7e3aedf13f0134e39a9e42b2980b4df5180409
Yosef Ashenafi <jossyfreelancer@gmail.com>
2024-10-17 08:20:04 +0300

    Add device info (#9)
    ### Added device info menu for the expo project

---
+++ utils/enum/index.ts
@@ -0,0 +1,6 @@
+import { snakeCaseToCapitalize } from '../string';
+
+export function enumToStr(enumObj: any, val: number): string {
+    let name = enumObj[val] as string;
+    return snakeCaseToCapitalize(name);
+}
---
+++ utils/string/index.ts
@@ -0,0 +1,7 @@
+export function capitalize(name: string): string {
+    return `${name.charAt(0).toUpperCase()}${name.slice(1).toLowerCase()}`;
+}
+
+export function snakeCaseToCapitalize(name: string): string {
+    return name.split('_').map(capitalize).join(' ');
+}
