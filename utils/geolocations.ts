367d8a8677d52eef8365e60839b267992f2b15a2
Shreyas Telkar <shretel@gmail.com>
2025-06-17 17:30:46 -0700

    update name

--- utils/geolocations.ts
+++ utils/geolocations.ts
@@ -1,3 +1,4 @@
+import { IGeolocationFormData } from '@baws-api';
 import { MAPBOX_ACCESS_TOKEN, WGS84_A, WGS84_B } from '@htk/constants';
 import {
     ICoordinates,
@@ -77,7 +78,7 @@ export function haversineDistanceMeters(
  * @param trackPoints - Array of track points with lat/long coordinates
  * @returns ICoordinates containing the centroid's latitude and longitude, or null if array is empty
  */
-export function calculate_centroid(trackPoints: ICoordinates[]): ICoordinates | null {
+export function calculateCentroid(trackPoints: ICoordinates[]): ICoordinates | null {
     let coordinates: ICoordinates | null;
 
     if (trackPoints.length === 0) {
